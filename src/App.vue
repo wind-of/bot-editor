fb86219cedf1428cd5002c6f738f940ed0bcd733
wind-of <adequation@mail.ru>
2022-01-24 11:29:42 +0300

    preparations...

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="application">
+    <aside id="navigation">
+      <h1 class="application__title">BotEditor</h1>
+      <router-link to="/"><div class="navigation__button">Home</div></router-link>
+      <router-link to="/editor"><div class="navigation__button">Editor</div></router-link>
+    </aside>
+    <main id="main">
+      <ul class="app-tools"></ul>
+      <router-view/>
+    </main>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "./index.css";
+#application {
+  height: auto;
+  width: 100%;
+  
+  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
+
+  display: flex;
+
+  #navigation, #main {
+    box-sizing: border-box;
+    height: 100vh;
+  }
+  #navigation {
+    width: 10%;
+
+    background-color: #1e1e1e;
+  }
+  #main {
+    width: 85%;
+  }
 }
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.application__title {
+  margin: 25px 0 25px 30px;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  color: white;
+  font-size: 22px;
+  cursor: default;
+}
+.navigation__button {
+  padding: 8px 0 8px 30px;
+
+  color: white;
+  font-size: 18px;
+  transition: .2s ease;
+
+  &:hover {
+    background-color: #3858e9;
   }
 }
 </style>
---
+++ src/views/Editor.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="editor">
+    <h1>Editor Page</h1>
+  </div>
+</template>
